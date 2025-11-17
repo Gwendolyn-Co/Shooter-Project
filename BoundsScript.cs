ab319f9e0a74ac73603871b43f7ecd8b8d612f8f
Arash <114724944+ArisenAce@users.noreply.github.com>
2025-11-17 15:53:31 -0500

    Delete BoundsScript.cs

--- BoundsScript.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Bounds : MonoBehaviour {
-    private void OnTriggerEnter2D(Collider2D other) {
-        Destroy(other.gameObject);
-    }
-}
