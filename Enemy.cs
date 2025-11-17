5391c1b6dab3fd2ae1d1ff654ae728156af5b774
Arash <114724944+ArisenAce@users.noreply.github.com>
2025-11-17 15:38:35 -0500

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -14,9 +14,28 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
-        if (transform.position.y < -6.5f)
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 7)
         {
-            Destroy(this.gameObject);
+            Player player = other.GetComponent<Player>();
+
+            if (player != null)
+            {
+                if (player.hasShield)
+                {
+                    // use up the shield instead of killing the player
+                    player.ConsumeShield();
+                }
+                else
+                {
+                    // no shield: normal death behavior
+                    Destroy(other.gameObject);
+                }
+            }
         }
     }
+
 }
